f13d3d5a0d41937ccddbe22d3187c8e1619bf5c4
Abishek0802 <abishek20td0802@gmail.com>
2023-12-30 19:34:58 +0530

    ..

--- myapp/src/ProfileUpdate/UpdateOwner.js
+++ myapp/src/ProfileUpdate/UpdateOwner.js
@@ -1,27 +1,40 @@
-import React, { useState } from 'react';
-import AddImg from "./AddImg.js"
-import { Card, CardContent, CardHeader, Button, Avatar, TextField } from '@mui/material';
+import React, { useState } from "react";
+import AddImg from "./AddImg.js";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Button,
+  Avatar,
+  TextField,
+} from "@mui/material";
 
 const MedicalShopOwnerProfileCard = () => {
-  const [name, setName] = useState('Medical Shop Owner Name');
-  const [number, setNumber] = useState('+1234567890');
-  const [address, setAddress] = useState('123 Medical Street, City');
-  const [experience, setExperience] = useState('X years');
-  const [image, ] = useState('path/to/your/image.jpg');
+  const [name, setName] = useState("Medical Shop Owner Name");
+  const [number, setNumber] = useState("+1234567890");
+  const [address, setAddress] = useState("123 Medical Street, City");
+  const [experience, setExperience] = useState("X years");
+  const [image] = useState("path/to/your/image.jpg");
 
   const handleUpdateProfile = () => {
     // Implement your logic to update the profile
-    console.log('Profile Updated:', { name, number, address, experience, image });
+    console.log("Profile Updated:", {
+      name,
+      number,
+      address,
+      experience,
+      image,
+    });
   };
 
   return (
-    <Card>
+    <Card style={{ width: "50%" }}>
       <CardHeader
         avatar={
           <Avatar
             alt="Medical Shop Owner"
             src={image}
-            style={{ borderRadius: '50%', width: '80px', height: '80px' }}
+            style={{ borderRadius: "50%", width: "80px", height: "80px" }}
           />
         }
         title={name}
@@ -29,41 +42,42 @@ const MedicalShopOwnerProfileCard = () => {
       />
       <CardContent>
         <TextField
-         sx={{paddingTop:"20px"}}
-         
+          sx={{ paddingTop: "20px" }}
           variant="outlined"
           fullWidth
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
         <TextField
-         sx={{paddingTop:"20px"}}
-        
+          sx={{ paddingTop: "20px" }}
           variant="outlined"
           fullWidth
           value={number}
           onChange={(e) => setNumber(e.target.value)}
         />
         <TextField
-         sx={{paddingTop:"20px"}}
-          
+          sx={{ paddingTop: "20px" }}
           variant="outlined"
           fullWidth
           value={address}
           onChange={(e) => setAddress(e.target.value)}
         />
         <TextField
-         sx={{paddingTop:"20px"}}
-        
+          sx={{ paddingTop: "20px" }}
           variant="outlined"
           fullWidth
           value={experience}
           onChange={(e) => setExperience(e.target.value)}
         />
-        <AddImg/>
+        <AddImg />
       </CardContent>
       <CardContent>
-        <Button size="large" variant="contained" color="primary" onClick={handleUpdateProfile}>
+        <Button
+          size="large"
+          variant="contained"
+          color="primary"
+          onClick={handleUpdateProfile}
+        >
           Update Profile
         </Button>
       </CardContent>
